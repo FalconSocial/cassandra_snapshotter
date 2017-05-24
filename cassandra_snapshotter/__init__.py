d7b78ff711806adb8af5efaedb764c61745c0828
Szilard Szabo <szilard.szabo@falconsocial.com>
2017-05-24 09:42:08 +0200

    Change version, maintainer, email

--- cassandra_snapshotter/__init__.py
+++ cassandra_snapshotter/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '1.0.0'
-__maintainer__ = 'Tommaso Barbugli'
-__email__ = 'tbarbugli@gmail.com'
+__version__ = '1.1.0'
+__maintainer__ = 'Szilard Szabo'
+__email__ = 'szilard.szabo@falcon.io'
